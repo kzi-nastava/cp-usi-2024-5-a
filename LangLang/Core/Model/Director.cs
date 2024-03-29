a639d0625ff630c2fdaa6cbf3a894d82d4b4b805
Natasa Kasikovic~ <nkasikovic@yahoo.com>
2024-03-29 13:39:18 +0100

    [Add] Director class implementation to the Model

---
+++ LangLang/Core/Model/Director.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using LangLang.Core.Repository.Serialization;
+
+namespace LangLang.Core.Model
+{
+    class Director : ISerializable
+    {
+
+        private Profile _profile;
+
+        public Profile Profile
+        {
+            get {return _profile;}
+            set { _profile = value;}
+        }
+
+        public Director(string name, string lastName, UserGender gender, DateTime dateOfBirth, string phoneNumber, string email, string password, UserType role) {
+            _profile = new Profile(name, lastName, gender, dateOfBirth, phoneNumber, email, password, role);
+        }
+
+        public string[] ToCSV()
+        {
+            string[] csvValues = { _profile.ToString() };
+            return csvValues;
+        }
+
+        public void FromCSV(string[] values)
+        {
+            _profile = new Profile(values[0], values[1], values[2], values[3], values[4], values[5], values[6], values[7]); // TODO: when implemented in Profile, consider how to catch an exception if necessary
+       
+        }
+    }
+}
