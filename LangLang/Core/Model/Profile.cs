0f14affae806fc3d8db3d782249c815086daf9ab
Ana Šinik <sinikana7@gmail.com>
2024-03-29 09:23:08 +0100

    [Update] Implementation of class Profile

--- LangLang/Core/Model/Profile.cs
+++ LangLang/Core/Model/Profile.cs
@@ -9,62 +9,62 @@ namespace LangLang.Core.Model
     class Profile
     {
         // Attributes
-        private string name;
-        private string lastName;
-        private UserGender gender;
-        private DateTime dateOfBirth;
-        private string phoneNumber;
-        private string email;
-        private string password;
-        private UserType role;
+        private string _name;
+        private string _lastName;
+        private UserGender _gender;
+        private DateTime _dateOfBirth;
+        private string _phoneNumber;
+        private string _email;
+        private string _password;
+        private UserType _role;
 
         // Properties
         public string Name
         {
-            get { return name; }
-            set { name = value; }
+            get { return _name; }
+            set { _name = value; }
         }
 
         public string LastName
         {
-            get { return lastName; }
-            set { lastName = value; }
+            get { return _lastName; }
+            set { _lastName = value; }
         }
 
         public UserGender Gender
         {
-            get { return gender; }
-            set { gender = value; }
+            get { return _gender; }
+            set { _gender = value; }
         }
 
         public DateTime DateOfBirth
         {
-            get { return dateOfBirth; }
-            set { dateOfBirth = value; }
+            get { return _dateOfBirth; }
+            set { _dateOfBirth = value; }
         }
 
         public string PhoneNumber
         {
-            get { return phoneNumber; }
-            set { phoneNumber = value; }
+            get { return _phoneNumber; }
+            set { _phoneNumber = value; }
         }
 
         public string Email
         {
-            get { return email; }
-            set { email = value; }
+            get { return _email; }
+            set { _email = value; }
         }
 
         public string Password
         {
-            get { return password; }
-            set { password = value; }
+            get { return _password; }
+            set { _password = value; }
         }
 
         public UserType Role
         {
-            get { return role; }
-            set { role = value; }
+            get { return _role; }
+            set { _role = value; }
         }
 
         // Constructor
@@ -78,6 +78,11 @@ namespace LangLang.Core.Model
             Email = email;
             Password = password;
             Role = role;
+        }
+
+        public override string ToString()
+        {
+            return string.Join("|", new object[] { Name, LastName, Gender, DateOfBirth.ToString("yyyy-MM-dd"), PhoneNumber, Email, Password, Role });
         }
 
     }
