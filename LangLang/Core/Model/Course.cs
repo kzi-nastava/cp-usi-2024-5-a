285d82222b0524b772ed6049678a93aa8e3271f0
Darinka Loncar <149894872+darinkaloncar@users.noreply.github.com>
2024-03-29 03:13:32 -0700

    [Update] private attributes, getters and setters in Course class

--- LangLang/Core/Model/Course.cs
+++ LangLang/Core/Model/Course.cs
@@ -10,54 +10,68 @@ namespace LangLang.Core.Model
     class Course
     {
         // Attributes
-        private string language;
-        private LanguageLevel level;
-        private int numberOfWeeks;
-        private List<WeekDays> days;
-        private bool online;
-        private int numberOfStudents;
-        private int maxStudents;
+        private int _id;
+        private string _language;
+        private LanguageLevel _level;
+        private int _numberOfWeeks;
+        private List<WeekDays> _days;
+        private bool _online;
+        private int _numberOfStudents;
+        private int _maxStudents;
+        private DateTime _creationDate;
 
         // Properties
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
         public string Language
         {
-            get { return language; }
-            set { language = value; }
+            get { return _language; }
+            set { _language = value; }
         }
 
         public LanguageLevel Level
         {
-            get { return level; }
-            set { level = value; }
+            get { return _level; }
+            set { _level = value; }
         }
      
         public int NumberOfWeeks
         {
-            get { return numberOfWeeks; }
-            set { numberOfWeeks = value; }
+            get { return _numberOfWeeks; }
+            set { _numberOfWeeks = value; }
         }
         public List<WeekDays> Days
         {
-            get { return days; }
-            set { days = value; }
+            get { return _days; }
+            set { _days = value; }
         }
         
         public bool Online
         {
-            get { return online; }
-            set { online = value; }
+            get { return _online; }
+            set { _online = value; }
         }
 
         public int NumberOfStudents
         {
-            get { return numberOfStudents; }
-            set { numberOfStudents = value; }
+            get { return _numberOfStudents; }
+            set { _numberOfStudents = value; }
         }
 
         public int MaxStudents
         {
-            get { return maxStudents; }
-            set { maxStudents = value; }
+            get { return _maxStudents; }
+            set { _maxStudents = value; }
+        }
+
+        public DateTime CreationDate 
+        {
+            get { return _creationDate; }
+            set { _creationDate = value; }
         }
 
         // Constructors
@@ -71,6 +85,7 @@ namespace LangLang.Core.Model
             Online = online;
             NumberOfStudents = numberOfStudents;
             MaxStudents = maxStudents;
+            CreationDate = DateTime.Now;
         }
 
         public Course()
