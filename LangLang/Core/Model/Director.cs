963a79aa7ea5550ccc02d20d58f6299b34e0b5ec
Natasa Kasikovic~ <nkasikovic@yahoo.com>
2024-03-30 16:30:14 +0100

    [Update] Implementation of Director class

--- LangLang/Core/Model/Director.cs
+++ LangLang/Core/Model/Director.cs
@@ -14,8 +14,8 @@ namespace LangLang.Core.Model
             set { _profile = value;}
         }
 
-        public Director(string name, string lastName, UserGender gender, DateTime dateOfBirth, string phoneNumber, string email, string password, UserType role) {
-            _profile = new Profile(name, lastName, gender, dateOfBirth, phoneNumber, email, password, role);
+        public Director(int id, string name, string lastName, UserGender gender, DateTime dateOfBirth, string phoneNumber, string email, string password, UserType role) {
+            _profile = new Profile(id, name, lastName, gender, dateOfBirth, phoneNumber, email, password, role);
         }
 
         public string[] ToCSV()
@@ -26,8 +26,16 @@ namespace LangLang.Core.Model
 
         public void FromCSV(string[] values)
         {
-            _profile = new Profile(values[0], values[1], values[2], values[3], values[4], values[5], values[6], values[7]); // TODO: when implemented in Profile, consider how to catch an exception if necessary
-       
+            try {
+                _profile = new Profile(values[0], values[1], values[2], values[3], values[4], values[5], values[6], values[7], values[8]);
+            } catch {
+                throw;
+            }
+        }
+
+        public int Id
+        {
+            get { return Profile.Id; }
         }
     }
 }
