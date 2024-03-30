da3d960934ff01ec53dce2dba2b07a0328bf8d5c
Ana Šinik <sinikana7@gmail.com>
2024-03-30 10:47:11 +0100

    [Add] Implementation of StudentController class

---
+++ LangLang/Core/Controller/StudentController.cs
@@ -0,0 +1,37 @@
+﻿using LangLang.Core.Model.DAO;
+using LangLang.Core.Model;
+using System.Collections.Generic;
+using LangLang.Core.Observer;
+
+namespace LangLang.Core.Controller
+{
+    public class StudentController
+    {
+        private readonly StudentDAO _students;
+
+        public StudentController()
+        {
+            _students = new StudentDAO();
+        }
+
+        public List<Student> GetAllStudents()
+        {
+            return _students.GetAllStudents();
+        }
+
+        public void Add(Student student)
+        {
+            _students.AddStudent(student);
+        }
+
+        public void Delete(int studentId)
+        {
+            _students.RemoveStudent(studentId);
+        }
+
+        public void Subscribe(IObserver observer)
+        {
+            _students.Subscribe(observer);
+        }
+    }
+}
