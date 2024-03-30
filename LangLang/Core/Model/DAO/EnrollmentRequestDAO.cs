ccc2a51d669e14d132eadb17b13a9cb4948f9100
Ana Šinik <sinikana7@gmail.com>
2024-03-30 17:17:40 +0100

    [Add] Implementation of EnrollmentRequestDAO class

---
+++ LangLang/Core/Model/DAO/EnrollmentRequestDAO.cs
@@ -0,0 +1,65 @@
+﻿using LangLang.Core.Observer;
+using LangLang.Core.Repository;
+using System.Collections.Generic;
+
+namespace LangLang.Core.Model.DAO
+{
+    public class EnrollmentRequestDAO : Subject
+    {
+        private readonly Dictionary<int, EnrollmentRequest> _enrollmentRequests;
+        private readonly Repository<EnrollmentRequest> _repository;
+
+        public EnrollmentRequestDAO()
+        {
+            _repository = new Repository<EnrollmentRequest>("enrollmentRequests.csv");
+            _enrollmentRequests = _repository.Load();
+        }
+        private int GenerateId()
+        {
+            if (_enrollmentRequests.Count == 0) return 0;
+            return _enrollmentRequests.Count + 1;
+        }
+
+        public EnrollmentRequest? GetEnrollmentRequestById(int id)
+        {
+            return _enrollmentRequests[id];
+        }
+
+        public Dictionary<int, EnrollmentRequest> GetAllEnrollmentRequests()
+        {
+            return _enrollmentRequests;
+        }
+
+        public EnrollmentRequest Add(EnrollmentRequest enrollmentRequest)
+        {
+            enrollmentRequest.Id = GenerateId();
+            _enrollmentRequests.Add(enrollmentRequest.Id, enrollmentRequest);
+            _repository.Save(_enrollmentRequests);
+            NotifyObservers();
+            return enrollmentRequest;
+        }
+
+        public EnrollmentRequest? Update(EnrollmentRequest enrollmentRequest)
+        {
+            EnrollmentRequest oldRequest = GetEnrollmentRequestById(enrollmentRequest.Id);
+            if (oldRequest == null) return null;
+
+            oldRequest.ERStatus = enrollmentRequest.ERStatus;
+
+            _repository.Save(_enrollmentRequests);
+            NotifyObservers();
+            return oldRequest;
+        }
+
+        public EnrollmentRequest? Remove(int id)
+        {
+            EnrollmentRequest enrollmentRequest = GetEnrollmentRequestById(id);
+            if (enrollmentRequest == null) return null; 
+
+            _enrollmentRequests.Remove(enrollmentRequest.Id);
+            _repository.Save(_enrollmentRequests);
+            NotifyObservers();
+            return enrollmentRequest;
+        }
+    }
+}
