80f95a228777ab949c79ab012ee5e47486453045
Darinka Loncar <149894872+darinkaloncar@users.noreply.github.com>
2024-03-30 14:09:56 +0100

    Revert "Revert "[Add] CourseController Class""
    This reverts commit 9348012efc05ccdac2edcecba2ee179fa6947166.

---
+++ LangLang/Core/Controller/CourseController.cs
@@ -0,0 +1,36 @@
+using LangLang.Core.Model;
+using LangLang.Core.Model.DAO;
+
+namespace LangLang.Core.Controller
+{
+    public class CourseController
+    {
+        private readonly CoursesDAO _courses;
+
+        public CourseController()
+        {
+            _courses = new CoursesDAO();
+        }
+
+        public List<Course> GetAllVehicles()
+        {
+            return _courses.GetAllCourses();
+        }
+
+        public void Add(Course course)
+        {
+            _courses.AddCourse(course);
+        }
+
+        public void Delete(int courseId)
+        {
+            _courses.RemoveCourse(courseId);
+        }
+
+        public void Subscribe(IObserver observer)
+        {
+            _courses.Subscribe(observer);
+        }
+
+    }   
+}
