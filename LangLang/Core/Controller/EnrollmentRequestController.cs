a9e8d5222fd675c175a043a5f4b891182a68eaa5
Ana Šinik <sinikana7@gmail.com>
2024-03-30 17:46:13 +0100

    [Add] Implementation of EnrollmentRequestController

---
+++ LangLang/Core/Controller/EnrollmentRequestController.cs
@@ -0,0 +1,38 @@
+﻿
+using LangLang.Core.Model.DAO;
+using System.Collections.Generic;
+using LangLang.Core.Model;
+using LangLang.Core.Observer;
+
+namespace LangLang.Core.Controller
+{
+    public class EnrollmentRequestController
+    {
+        private readonly EnrollmentRequestDAO _enrollmentRequests;
+
+        public EnrollmentRequestController()
+        {
+            _enrollmentRequests = new EnrollmentRequestDAO();
+        }
+
+        public Dictionary<int, EnrollmentRequest> GetAll()
+        {
+            return _enrollmentRequests.GetAllEnrollmentRequests();
+        }
+
+        public void Add(EnrollmentRequest enrollmentRequest)
+        {
+            _enrollmentRequests.Add(enrollmentRequest);
+        }
+
+        public void Delete(int id)
+        {
+            _enrollmentRequests.Remove(id);
+        }
+
+        public void Subscribe(IObserver observer)
+        {
+            _enrollmentRequests.Subscribe(observer);
+        }
+    }
+}
