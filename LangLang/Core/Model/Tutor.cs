fd0b6f52f2c44a1e2b443becaff21c4fe40c68be
Natasa Kasikovic~ <nkasikovic@yahoo.com>
2024-03-30 01:33:18 +0100

    [Add] Implementation of constructor without params

--- LangLang/Core/Model/Tutor.cs
+++ LangLang/Core/Model/Tutor.cs
@@ -5,14 +5,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Data;
+using System.Reflection;
 
 namespace LangLang.Core.Model
 {
     public class Tutor : ISerializable
     {
-        private Profile _profile { get; set; }
-        private Dictionary<string, LanguageLevel> _languageSkills { get; set; }
-        private DateTime _employmentDate { get; set; }
+        private Dictionary<string, LanguageLevel> _languageSkills;
+
+        private DateTime _employmentDate;
+
+        private Profile _profile;
+
+        public Dictionary<string, LanguageLevel> LanguageSkills
+        {
+            get { return _languageSkills; }
+            set { _languageSkills = value; }
+        }
+        public DateTime EmploymentDate
+        {
+            get { return _employmentDate; }
+            set { _employmentDate = value; }
+        }
+        public Profile Profile
+        {
+            get { return _profile; }
+            private set { _profile = value; }
+        }
 
         public Tutor(int id, string name, string lastName, UserGender gender, DateTime dateOfBirth, string phoneNumber, string email, string password, UserType role, DateTime employmentDate, Dictionary<string, LanguageLevel> languageSkills) {
             _profile = new Profile(id, name, lastName, gender, dateOfBirth, phoneNumber, email, password, role);
@@ -20,16 +39,21 @@ namespace LangLang.Core.Model
             _languageSkills = languageSkills;
             }
 
+        public Tutor()
+        {
+            _languageSkills = new Dictionary<string, LanguageLevel>();
+            _employmentDate = DateTime.MinValue;
+            _profile = new();
+        }
+
         public void FromCSV(string[] values)
         {
             _profile = new Profile(values[0], values[1], values[2], values[3], values[4], values[5], values[6], values[7], values[8]);
-            DateTime employmentDate;
 
-            if (!DateTime.TryParseExact(values[9], "yyyy-MM-dd", null, System.Globalization.DateTimeStyles.None, out employmentDate))
+            if (!DateTime.TryParseExact(values[9], "yyyy-MM-dd", null, System.Globalization.DateTimeStyles.None, out _employmentDate))
             {
                 throw new FormatException("Date is not in the correct format.");
             }
-            _employmentDate = employmentDate;
 
             for (int i = 10; i < values.Length; i++)
             {
@@ -59,15 +83,19 @@ namespace LangLang.Core.Model
             return csvValues;
         }
 
-        public string DictToCSV(Dictionary<string, LanguageLevel> skills){
+        public string DictToCSV(Dictionary<string, LanguageLevel> skills)
+        {
             StringBuilder sb = new StringBuilder();
             var length = skills.Count();
-            foreach (var skill in skills){
+            foreach (var skill in skills)
+            {
                 sb.Append(skill.Key).Append(",").Append(skill.Value);
-                if (--length>0) sb.Append("|");
+                if (--length > 0) sb.Append("|");
             }
             return sb.ToString();
         }
+
+
 
         /*
         public List<Course> GetCourses(ref Dictionary<int, Course> courses)
