1689ac2c5fff5435f7f0b3875d19ae5d0a2426ed
DusicaPesic <130140285+DusicaPesic@users.noreply.github.com>
2024-04-02 18:28:06 +0200

    [Update] fixed bug while creating exam slot

--- LangLang/View/ExamSlotGUI/ExamSlotCreateWindow.xaml.cs
+++ LangLang/View/ExamSlotGUI/ExamSlotCreateWindow.xaml.cs
@@ -32,6 +32,7 @@ namespace LangLang.View
         public ExamSlotCreateWindow(Dictionary<int,Course> courses,ExamSlotController controller)
         {
             Courses = courses.Values.ToList<Course>();
+            Trace.WriteLine("U create " + Courses.Count);
             SelectedCourse = new Course();
             examSlotsController = controller;
             ExamSlot = new ExamSlotDTO();
@@ -43,7 +44,6 @@ namespace LangLang.View
 
         private void examSlotCreateBtn_Click(object sender, RoutedEventArgs e)
         {
-            Trace.WriteLine("Mas "+ExamSlot.MaxStudents);
             if (ExamSlot.IsValid)
             {
                 examSlotsController.Add(ExamSlot.ToExamSlot());
@@ -64,6 +64,7 @@ namespace LangLang.View
                 ExamSlot.Language = SelectedCourse.Language;
                 levelTb.Text = SelectedCourse.Level.ToString();
                 ExamSlot.Level = SelectedCourse.Level;
+                ExamSlot.CourseId = SelectedCourse.Id;
                 //CoursesDataGrid.SelectedItem = null;
             }
         }
