84ea279dd8ecda43db6b08c5c825f793e5a77e9f
Natasa Kasikovic~ <nkasikovic@yahoo.com>
2024-04-28 14:22:27 +0200

    [Refactor]: ExamSlot is independent of a specific course instance

--- LangLang/Core/Model/ExamSlot.cs
+++ LangLang/Core/Model/ExamSlot.cs
@@ -1,104 +1,52 @@
 ﻿using LangLang.Core.Repository.Serialization;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LangLang.Core.Model
 {
     public class ExamSlot: ISerializable
     {
-        // Private attributes
-        private int _id;
-        private int _courseId;
-        private int _maxStudents;
-        private DateTime _examDateTime;
-        private int _numberOfStudents;
+        public int Id { get; set; }
+        public string Language { get; set; }
+        public LanguageLevel Level { get; set; }
+        
+        // TODO: add tutor
 
-        // Public properties
-        public int Id
-        {
-            get { return _id; }
-            set { _id = value; }
-        }
+        public TimeSlot TimeSlot { get; set; }
+        public int MaxStudents { get; set; }
+        public int StudentsNumber { get; set; } // TODO: mozda ne treba broj prijavljenih nego mozes da pitas abzu
 
-        public int CourseId
+        public ExamSlot(int id, string language, LanguageLevel level, TimeSlot timeSlot, int maxStudents, DateTime examDateTime, int studentsNumber)
         {
-            get { return _courseId; }
-            set { _courseId = value; }
-        }
-
-        public int MaxStudents
-        {
-            get { return _maxStudents; }
-            set { _maxStudents = value; }
-        }
-
-        public DateTime ExamDateTime
-        {
-            get { return _examDateTime; }
-            set { _examDateTime = value; }
-        }
-
-        public int NumberOfStudents
-        {
-            get { return _numberOfStudents; }
-            set { _numberOfStudents = value; }
-        }
-
-        // Constructor
-        public ExamSlot(int id, int courseId, int maxStudents, DateTime examDateTime, int numberOfStudents)
-        {
-            _id = id;
-            _courseId = courseId;
-            _maxStudents = maxStudents;
-            _examDateTime = examDateTime;
-            _numberOfStudents = numberOfStudents;
-        }
-
-        public ExamSlot(int id, int courseId, int maxStudents, DateTime examDateTime)
-        {
-            _id = id;
-            _courseId = courseId;
-            _maxStudents = maxStudents;
-            _examDateTime = examDateTime;
-            _numberOfStudents = 0;
+            Id = id;
+            Language = language;
+            Level = Level;
+            TimeSlot = timeSlot;
+            MaxStudents = maxStudents;
+            StudentsNumber = studentsNumber;
         }
 
         public ExamSlot() { }
 
-        // Methods
-
-        // Method to display information about the exam slot
-        public override string ToString()
-        {
-            return $"ID: {_id} | Course: {_courseId.ToString()} | Max Students: {_maxStudents} | Exam Date and Time: {_examDateTime} | Number of Students: {_numberOfStudents}";
-        }
-
-        // Method to convert object to CSV format
-        //returns array of strings representing attributes of ExamSlot object in string form
         public string[] ToCSV()
         {
-            string[] csvValues =
+            return new string[]
             {
-            _id.ToString(),
-            _courseId.ToString(),
-            _maxStudents.ToString(),
-            _examDateTime.ToString(),
-            _numberOfStudents.ToString()
-        };
-            return csvValues;
+            Id.ToString(),
+            Language,
+            Level.ToString(),
+            MaxStudents.ToString(),
+            TimeSlot.ToString(),
+            StudentsNumber.ToString()
+            };
         }
-        // Method to make object from CSV data
-        //takes array of strings representing ExamSlot object values and sets attributes of object to that values
         public void FromCSV(string[] values)
         {
-            _id = int.Parse(values[0]);
-            _courseId = int.Parse(values[1]);
-            _maxStudents = int.Parse(values[2]);
-            _examDateTime = DateTime.Parse(values[3]);
-            _numberOfStudents = int.Parse(values[4]);
+            Id = int.Parse(values[0]);
+            Language = values[1];
+            Level = (LanguageLevel)Enum.Parse(typeof(LanguageLevel), values[2]);
+            MaxStudents = int.Parse(values[3]);
+            // TODO: add exam slot
+            StudentsNumber = int.Parse(values[5]);
         }
     }
 }
