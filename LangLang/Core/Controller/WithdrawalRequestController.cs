5986dfce4d102fd49ced3fc56d9d72a44655e809
Ana Šinik <sinikana7@gmail.com>
2024-04-29 15:55:40 +0200

    [feat] Implement WithdrawalRequest functionality: added model, DAO and Controller classes

---
+++ LangLang/Core/Controller/WithdrawalRequestController.cs
@@ -0,0 +1,44 @@
+﻿
+using LangLang.Core.Model;
+using LangLang.Core.Model.DAO;
+using LangLang.Core.Observer;
+using System.Collections.Generic;
+
+namespace LangLang.Core.Controller
+{
+    public class WithdrawalRequestController
+    {
+        private readonly WithdrawalRequestDAO _withdrawalRequests;
+
+        public WithdrawalRequestController()
+        {
+            _withdrawalRequests = new();
+        }
+
+        public List<WithdrawalRequest> GetAll()
+        {
+            return _withdrawalRequests.GetAllWithdrawalRequests();
+        }
+
+        public void Add(WithdrawalRequest withdrawalRequest)
+        {
+            _withdrawalRequests.Add(withdrawalRequest);
+        }
+
+        public void Delete(int id)
+        {
+            _withdrawalRequests.Remove(id);
+        }
+
+        public void Subscribe(IObserver observer)
+        {
+            _withdrawalRequests.Subscribe(observer);
+        }
+
+        public List<WithdrawalRequest> GetStudentRequests(int studentId, EnrollmentRequestController erController)
+        {
+            List<EnrollmentRequest> allEnrollmentRequests = erController.GetAll();
+            return _withdrawalRequests.GetStudentRequests(studentId, allEnrollmentRequests);
+        }
+    }
+}
