9f02ec39564bfe519c8550227815698c421afb41
Natasa Kasikovic~ <nkasikovic@yahoo.com>
2024-05-01 01:12:40 +0200

    [feat] add isDeleted attribute to Profile

--- LangLang/Core/Model/Profile.cs
+++ LangLang/Core/Model/Profile.cs
@@ -20,8 +20,9 @@ namespace LangLang.Core.Model
         public string Email { get; set; }
         public string Password { get; set; }    
         public UserType Role { get; set; }
+        public bool IsDeleted { get; set; }
 
-        public Profile(int id, string name, string lastName, Gender gender, DateTime birthDate, string phoneNumber, string email, string password, UserType role)
+        public Profile(int id, string name, string lastName, Gender gender, DateTime birthDate, string phoneNumber, string email, string password, UserType role, bool isDeleted)
         {
             Id = id;
             Name = name;
@@ -32,13 +33,14 @@ namespace LangLang.Core.Model
             Email = email;
             Password = password;
             Role = role;
+            IsDeleted = isDeleted;
         }
 
         public Profile() { }
 
         /// Constructor for initializing after parsing data loaded from file.
         /// <exception cref="FormatException">Thrown when date is not in the correct format.</exception>
-        public Profile(string id, string name, string lastName, string gender, string birthDate, string phoneNumber, string email, string password, string role)
+        public Profile(string id, string name, string lastName, string gender, string birthDate, string phoneNumber, string email, string password, string role, string isDeleted)
         {
             try {
                 BirthDate = DateTime.ParseExact(birthDate, "yyyy-MM-dd", null);
@@ -55,11 +57,12 @@ namespace LangLang.Core.Model
             PhoneNumber = phoneNumber;
             Email = email; 
             Password = password;
+            IsDeleted = bool.Parse(isDeleted);
         }
 
         public override string ToString()
         {
-            return string.Join("|", new object[] { Id, Name, LastName, Gender, BirthDate.ToString("yyyy-MM-dd"), PhoneNumber, Email, Password, Role });
+            return string.Join("|", new object[] { Id, Name, LastName, Gender, BirthDate.ToString("yyyy-MM-dd"), PhoneNumber, Email, Password, Role, IsDeleted });
         }
 
     }
