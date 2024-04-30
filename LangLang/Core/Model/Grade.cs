65778406c723427d2afcb4e3ab6aed7b7ba60be3
Darinka Loncar <149894872+darinkaloncar@users.noreply.github.com>
2024-04-30 12:35:23 -0700

    [Add] Grade Class
    Implement class for grades.

---
+++ LangLang/Core/Model/Grade.cs
@@ -0,0 +1,75 @@
+﻿using LangLang.Core.Repository.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LangLang.Core.Model
+{
+    public class Grade : ISerializable
+    {
+        private int _id;
+        private int _courseId;
+        private int _studentId;
+        private int _gradeValue;
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; } 
+        }
+        public int CourseId
+        { 
+          get { return _courseId; }
+          set { _courseId = value; } 
+        }
+        public int StudentId
+        { 
+          get { return _studentId; }
+          set { _studentId = value; } 
+        }
+        public int GradeValue
+        {
+            get => _gradeValue;
+            set
+            {
+                if (value < 1 || value > 10)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), "Value must be between 1 and 10.");
+                }
+                _gradeValue = value;
+            }
+        }
+
+        public Grade(int id, int courseId, int studentId, int gradeValue)
+        {
+            Id = id;
+            CourseId = courseId;
+            StudentId = studentId;
+            GradeValue = gradeValue;
+        }
+
+        public Grade() { }
+
+        public string[] ToCSV()
+        {
+            string[] csvValues = { _id.ToString(), _courseId.ToString(), _studentId.ToString(), _gradeValue.ToString() };
+            return csvValues;
+        }
+
+        public void FromCSV(string[] values)
+        {
+            try
+            {
+                _id = int.Parse(values[0]);
+                _courseId = int.Parse(values[1]);
+                _studentId = int.Parse(values[2]);
+                _gradeValue = int.Parse(values[3]);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
