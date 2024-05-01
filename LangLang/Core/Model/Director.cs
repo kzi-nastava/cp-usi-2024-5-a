ac110b5204924af31e4cc39a50a67110058d027f
Natasa Kasikovic~ <nkasikovic@yahoo.com>
2024-05-01 12:59:05 +0200

    [refactor] director class

--- LangLang/Core/Model/Director.cs
+++ LangLang/Core/Model/Director.cs
@@ -5,39 +5,25 @@ namespace LangLang.Core.Model
 {
     public class Director : ISerializable
     {
+        public Profile Profile{ get; set; }
+        public int Id => Profile.Id;
 
-        private Profile _profile;
-
-        public Profile Profile
-        {
-            get {return _profile;}
-            set { _profile = value;}
-        }
-
-        public Director(int id, string name, string lastName, Gender gender, DateTime dateOfBirth, string phoneNumber, string email, string password, UserType role, bool isDeleted) {
-            _profile = new Profile(id, name, lastName, gender, dateOfBirth, phoneNumber, email, password, role, isDeleted);
+        public Director(int id, string name, string lastName, Gender gender, DateTime birthDate, string phoneNumber, string email, string password, UserType role, bool isDeleted) {
+            Profile = new (id, name, lastName, gender, birthDate, phoneNumber, email, password, role, isDeleted);
         }
 
         public Director() { }
 
         public string[] ToCSV()
         {
-            string[] csvValues = { _profile.ToString() };
-            return csvValues;
+            return new string[]{ 
+                Profile.ToString()};
         }
 
         public void FromCSV(string[] values)
         {
-            try {
-                _profile = new Profile(values[0], values[1], values[2], values[3], values[4], values[5], values[6], values[7], values[8], values[9]);
-            } catch {
-                throw;
-            }
+            Profile = new (values[0], values[1], values[2], values[3], values[4], values[5], values[6], values[7], values[8], values[9]);
         }
 
-        public int Id
-        {
-            get { return Profile.Id; }
-        }
     }
 }
