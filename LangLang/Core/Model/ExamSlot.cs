3d4042efbd8ea8cab4349900a2670d4935a8a9d2
Natasa Kasikovic~ <nkasikovic@yahoo.com>
2024-05-03 01:05:14 +0200

    [feat] an attribute has been added to track the number of registrations per exam

--- LangLang/Core/Model/ExamSlot.cs
+++ LangLang/Core/Model/ExamSlot.cs
@@ -13,19 +13,18 @@ namespace LangLang.Core.Model
         public TimeSlot TimeSlot { get; set; }
         public int MaxStudents { get; set; }
         public bool Modifiable { get; set; }
+        public int Applicants { get; set; }
 
-        // NOTE: if possible don't save number of registeredStudents, ask the database. If not, then add attribute.
-        
-
-        public ExamSlot(int id, string language, LanguageLevel level, TimeSlot timeSlot, int maxStudents, int tutorId, bool modifiable)
+        public ExamSlot(int id, string language, LanguageLevel level, TimeSlot timeSlot, int maxStudents, int tutorId, bool modifiable, int applicants)
         {
             Id = id;
-            Language = language;
+            Language = language;   
             Level = Level;
             TutorId = tutorId;
             TimeSlot = timeSlot;
             MaxStudents = maxStudents;
             Modifiable = modifiable;
+            Applicants = applicants;
         }
 
         public ExamSlot() { }
@@ -39,6 +38,7 @@ namespace LangLang.Core.Model
             TutorId.ToString(),
             TimeSlot.ToString(),
             MaxStudents.ToString(),
+            Applicants.ToString(),
             Modifiable.ToString()
             };
         }
@@ -51,7 +51,8 @@ namespace LangLang.Core.Model
             TutorId = int.Parse(values[3]);
             TimeSlot = new (values[4], values[5]);
             MaxStudents = int.Parse(values[6]);
-            Modifiable = bool.Parse(values[7]);
+            Applicants = int.Parse(values[7]);
+            Modifiable = bool.Parse(values[8]);
         }
 
         public bool ApplicationsVisible()
