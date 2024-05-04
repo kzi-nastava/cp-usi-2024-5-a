e2532febdc376261c8497c1c993dcaf76492324a
Natasa Kasikovic~ <nkasikovic@yahoo.com>
2024-05-04 14:53:40 +0200

    [feat] implement ExamResultController and ExamResultDAO

---
+++ LangLang/Core/Controller/ExamResultController.cs
@@ -0,0 +1,33 @@
+﻿using LangLang.Core.Model;
+using LangLang.Core.Model.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LangLang.Core.Controller
+{
+    public class ExamResultController
+    {
+        private readonly ExamResultDAO _examResults;
+
+        public ExamResultController() {
+            _examResults = new ();
+        }
+
+        public List<ExamResult> Get(int examId)
+        {
+            return _examResults.Get(examId);
+        }
+
+        public void GenerateResults(List<Student> students, int examId)
+        {
+            _examResults.GenerateResults(students, examId);
+        }
+        internal void Update(ExamResult examResult)
+        {
+            _examResults.Update(examResult);
+        }
+    }
+}
