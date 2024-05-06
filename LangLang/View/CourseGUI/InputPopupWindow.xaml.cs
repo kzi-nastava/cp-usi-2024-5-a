cfe5bcc8f3ac4bd4d3111fe89a27c9bbdddd4a1c
Darinka Loncar <149894872+darinkaloncar@users.noreply.github.com>
2024-05-05 17:23:25 -0700

    [Add] input popup to Course GUI
    This popup is used for tutor to input the reason behind rejecting an enrollment request.

---
+++ LangLang/View/CourseGUI/InputPopupWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LangLang.View.CourseGUI
+{
+    /// <summary>
+    /// Interaction logic for InputPopupWindow.xaml
+    /// </summary>
+    public partial class InputPopupWindow : Window
+    {
+        public InputPopupWindow()
+        {
+            InitializeComponent();
+        }
+        public string EnteredText { get; private set; }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            EnteredText = TextInput.Text;
+            Close();
+        }
+    }
+}
