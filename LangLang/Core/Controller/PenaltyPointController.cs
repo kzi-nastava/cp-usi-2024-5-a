27ce648d2434c000a88abc5481d6f18102877d34
DusicaPesic <130140285+DusicaPesic@users.noreply.github.com>
2024-05-07 22:32:11 +0200

    [Update] names in PenaltyPoint classes

--- LangLang/Core/Controller/PenaltyPointController.cs
+++ LangLang/Core/Controller/PenaltyPointController.cs
@@ -18,13 +18,13 @@ namespace LangLang.Core.Controller
             _points = new PenaltyPointDAO();
         }
 
-        public PenaltyPoint? GetById(int id)
+        public PenaltyPoint? Get(int id)
         {
-            return _points.GetById(id);
+            return _points.Get(id);
         }
-        public List<PenaltyPoint> GetAllPoints()
+        public List<PenaltyPoint> GetAll()
         {
-            return _points.GetAllPoints().Values.ToList();
+            return _points.GetAll().Values.ToList();
         }
         // Allows a tutor to assign a penalty point to a student for a specific course and deactivate the student's account if required
         public void GivePenaltyPoint(Student student, Tutor tutor, Course course, AppController appController)
@@ -43,9 +43,9 @@ namespace LangLang.Core.Controller
             _points.RemovePenaltyPoint(student);
         }
 
-        public bool HasAlreadyGivenPenaltyPoint(Student student, Tutor tutor, Course course, AppController appController)
+        public bool HasGivenPenaltyPoint(Student student, Tutor tutor, Course course, AppController appController)
         {
-            return _points.HasAlreadyGivenPenaltyPoint(student, tutor, course, appController);
+            return _points.HasGivenPenaltyPoint(student, tutor, course, appController);
         }
     }
 }
