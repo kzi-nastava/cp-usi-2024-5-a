6a2eea5e38b2ae18183d497ddbd342cea0c428a5
Darinka Loncar <149894872+darinkaloncar@users.noreply.github.com>
2024-05-21 16:30:00 -0700

    [Update] the position of LanguageLevel enum in project

---
+++ LangLang/Domain/Models/Enums/LanguageLevel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LangLang.Domain.Models.Enums
+{
+    public enum LanguageLevel
+    {
+        A1,
+        A2,
+        B1,
+        B2,
+        C1,
+        C2
+    }
+}
