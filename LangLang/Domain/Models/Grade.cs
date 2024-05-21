523de07bcffe263dfab2becb9112cbf4ed17943f
Darinka Loncar <149894872+darinkaloncar@users.noreply.github.com>
2024-05-21 16:28:48 -0700

    [Update] the position of Grade class in project

---
+++ LangLang/Domain/Models/Grade.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LangLang.Domain.Models
+{
+    public class Grade
+    {
+        public int Id { get; set; }
+        public int CourseId { get; set; }
+        public int StudentId { get; set; }
+        public int ActivityGrade { get; set; }
+        public int KnowledgeGrade { get; set; }
+
+        public Grade(int id, int courseId, int studentId, int activityGrade, int knowledgeGrade)
+        {
+            Id = id;
+            CourseId = courseId;
+            StudentId = studentId;
+            ActivityGrade = activityGrade;
+            KnowledgeGrade = knowledgeGrade;
+        }
+
+        public Grade() { }
+
+        public override string ToString()
+        {
+            return string.Join("|", new object[] { Id, CourseId, StudentId, ActivityGrade, KnowledgeGrade});
+        }
+    }
+}
