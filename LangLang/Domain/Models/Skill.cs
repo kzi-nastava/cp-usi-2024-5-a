a841f06afd3e5b800ee4a81c548c1afcbbc6db17
Darinka Loncar <149894872+darinkaloncar@users.noreply.github.com>
2024-05-24 17:06:05 -0700

    [Update] refactor skill class

--- LangLang/Domain/Models/Skill.cs
+++ LangLang/Domain/Models/Skill.cs
@@ -10,13 +10,13 @@ namespace LangLang.Domain.Models
 {
     public class Skill
     {
-        private List<string> _language;
-        private List<LanguageLevel> _level;
+        public List<string> Language { get; set; }
+        public List<LanguageLevel> Level { get; set; }
 
         public Skill()
         {
-            _language = new List<string>();
-            _level = new List<LanguageLevel>();
+            Language = new List<string>();
+            Level = new List<LanguageLevel>();
         }
         public Skill(List<string> language, List<LanguageLevel> level)
         {
@@ -24,16 +24,5 @@ namespace LangLang.Domain.Models
             Level = level;
         }
 
-        public List<string> Language
-        {
-            get { return _language; }
-            set { _language = value; }
-        }
-
-        public List<LanguageLevel> Level
-        {
-            get { return _level; }
-            set { _level = value; }
-        }
     }
 }
