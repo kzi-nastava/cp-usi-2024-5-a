aa14005c4b6b2ec171fb872709f5b3db8d2b3652
DusicaPesic <130140285+DusicaPesic@users.noreply.github.com>
2024-05-25 19:20:10 +0200

    [Add] ViewModel for tutors exams

---
+++ LangLang/WPF/ViewModels/ExamViewModels/ExamSlotsTutorViewModel.cs
@@ -0,0 +1,71 @@
+﻿using LangLang.BusinessLogic.UseCases;
+using LangLang.Core;
+using LangLang.Domain.Models;
+using LangLang.View.ExamSlotGUI;
+using LangLang.WPF.ViewModels.CourseViewModels;
+using LangLang.WPF.ViewModels.ExamViewModel;
+using LangLang.WPF.ViewModels.RequestsViewModels;
+using LangLang.WPF.Views.StudentView;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace LangLang.WPF.ViewModels.ExamViewModels
+{
+    public class ExamSlotsTutorViewModel
+    {
+        public ObservableCollection<ExamSlotViewModel> ExamSlots { get; set; }
+        public ExamSlotViewModel SelectedExamSlot { get; set; }
+        public Tutor LoggedIn { get;set; }
+        public ExamSlotsTutorViewModel(Tutor LoggedIn)
+        {
+            this.LoggedIn = LoggedIn;
+            ExamSlots = new ObservableCollection<ExamSlotViewModel>();
+            SetDataForReview();
+        }
+        public void SetDataForReview()
+        {
+            ExamSlots.Clear();
+            ExamSlotService examSlotService = new();
+
+            foreach (ExamSlot exam in examSlotService.GetExams(LoggedIn))
+            {
+                ExamSlots.Add(new ExamSlotViewModel(exam));
+            }
+        }
+  
+        public void UpdateExam()
+        {
+            ExamSlotService examSlotService = new();
+            ExamSlotUpdateWindow updateWindow = new(SelectedExamSlot.Id, LoggedIn);
+            if (examSlotService.CanBeUpdated(SelectedExamSlot.ToExamSlot()))
+                updateWindow.Show();
+            else
+                MessageBox.Show($"Can't update exam, there is less than {Constants.EXAM_MODIFY_PERIOD} days before exam or exam has passed.");
+        }
+
+        public void DeleteExam()
+        {
+            ExamSlotService examSlotService = new();
+            if (!examSlotService.Delete(SelectedExamSlot.Id))
+            {
+                MessageBox.Show($"Can't delete exam, there is less than {Constants.EXAM_MODIFY_PERIOD} days before exam.");
+            }
+            else
+            {
+                MessageBox.Show("Exam slot successfully deleted.");
+            }
+
+            SetDataForReview();
+        }
+        public bool IsExamSelected()
+        {
+            return SelectedExamSlot != null;
+        }
+
+    }
+}
---
+++ LangLang/WPF/Views/TutorView/Tabs/ExamsReview.xaml.cs
@@ -0,0 +1,80 @@
+﻿using LangLang.BusinessLogic.UseCases;
+using LangLang.Domain.Models;
+using LangLang.View;
+using LangLang.View.StudentGUI.Tabs;
+using LangLang.WPF.ViewModels.CourseViewModels;
+using LangLang.WPF.ViewModels.ExamViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LangLang.WPF.Views.TutorView.Tabs
+{
+    /// <summary>
+    /// Interaction logic for Exams.xaml
+    /// </summary>
+    public partial class ExamsReview : UserControl
+    {
+        public ExamSlotsTutorViewModel ExamsTutorVM { get; set; }
+        public ExamsReview(Tutor LoggedIn)
+        {
+            InitializeComponent();
+            ExamsTutorVM = new ExamSlotsTutorViewModel(LoggedIn);
+            DataContext = ExamsTutorVM;
+
+            Update();
+        }
+        public void Update()
+        {
+            ExamsTutorVM.SetDataForReview();
+        }
+        private void ExamSlotCreateWindowBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ExamSlotCreateWindow createWindow = new(ExamsTutorVM.LoggedIn,this);
+            createWindow.Show();
+        }
+        private void ExamSlotUpdateWindowBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ExamsTutorVM.UpdateExam();
+        }
+        private void ExamSlotDeleteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ExamsTutorVM.DeleteExam();
+        }
+        private void DisableButtonsES()
+        {
+            deleteExamBtn.IsEnabled = false;
+            updateExamBtn.IsEnabled = false;
+
+        }
+
+        private void EnableButtonsES()
+        {
+            deleteExamBtn.IsEnabled = true;
+            updateExamBtn.IsEnabled = true;
+
+        }
+        private void ExamSlotsDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (!ExamsTutorVM.IsExamSelected()) // when the DataGrid listener is triggered, check if there is a selection, and based on that, decide whether to enable or disable the buttons
+            {
+                DisableButtonsES();
+            }
+            else
+            {
+                EnableButtonsES();
+            }
+        }
+    }
+}
