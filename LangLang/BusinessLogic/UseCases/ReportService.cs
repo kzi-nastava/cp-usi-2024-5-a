da01d54b579072667899eda668bc819c3b707426
DusicaPesic <130140285+DusicaPesic@users.noreply.github.com>
2024-05-26 14:48:26 +0200

    [Add] methods for report C

--- LangLang/BusinessLogic/UseCases/ReportService.cs
+++ LangLang/BusinessLogic/UseCases/ReportService.cs
@@ -1,5 +1,6 @@
 ﻿
 using LangLang.Configuration;
+using LangLang.Domain.Enums;
 using LangLang.Domain.Models;
 using System.Collections.Generic;
 
@@ -46,5 +47,46 @@ namespace LangLang.BusinessLogic.UseCases
             List<Student> students = penaltyService.GetStudentsByPenaltyCount(course, points);
             return gradeService.GetAverageGrade(students, course);
         }
+
+        public float[] GetAverageResults()
+        {
+            ExamSlotService examsService = new();
+            ExamResultService resultsService = new();
+
+            float[] averages = new float[] { 0f, 0f, 0f, 0f };
+            List<ExamSlot> exams = examsService.GetExamsHeldInLastYear();
+            List<ExamResult> results = new List<ExamResult>();
+            results = resultsService.GetByExams(exams);
+
+            foreach(ExamResult result in results)
+            {
+                averages[0] += result.ReadingPoints;
+                averages[1] += result.SpeakingPoints;
+                averages[2] += result.WritingPoints;
+                averages[3] += result.ListeningPoints;
+            }
+            for (int i = 0; i < averages.Length; i++)
+            {
+                averages[i] /= results.Count;
+            }
+            return averages;
+        }
+        public int NumStudentsAttended(Course course)
+        {
+            CourseService coursesService = new();
+            return coursesService.NumStudentsAttended(course);
+        }
+        public int NumStudentsPassed(Course course)
+        {
+            CourseService coursesService = new();
+            return coursesService.NumStudentsPassed(course);
+        }
+        public double CalculatePassingPercentage(Course course)
+        {
+            int attended = NumStudentsAttended(course);
+            int passed = NumStudentsPassed(course);
+            return (passed / attended)*100;
+        }
+
     }
 }
