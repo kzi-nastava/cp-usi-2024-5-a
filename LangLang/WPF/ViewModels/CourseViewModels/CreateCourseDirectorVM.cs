2f118a5a89da7effc3826a2c9afb822c8e91427b
DusicaPesic <130140285+DusicaPesic@users.noreply.github.com>
2024-05-27 15:54:57 +0200

    [Add] smart tutor recommendation

--- LangLang/WPF/ViewModels/CourseViewModels/CreateCourseDirectorVM.cs
+++ LangLang/WPF/ViewModels/CourseViewModels/CreateCourseDirectorVM.cs
@@ -1,12 +1,6 @@
 ﻿using LangLang.BusinessLogic.UseCases;
 using LangLang.Configuration;
-using LangLang.WPF.ViewModels.TutorViewModels;
-using System;
-using System.CodeDom;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows;
 
 namespace LangLang.WPF.ViewModels.CourseViewModels
@@ -18,6 +12,7 @@ namespace LangLang.WPF.ViewModels.CourseViewModels
         public CreateCourseDirectorVM()
         {
             Course = new CourseViewModel();
+            Course.TutorId = Constants.DELETED_TUTOR_ID;
         }
 
         public bool CreatedCourse()
@@ -31,7 +26,8 @@ namespace LangLang.WPF.ViewModels.CourseViewModels
             var courseService = new CourseService();
             if (courseService.CanCreateOrUpdate(Course.ToCourse()))
             {
-                //Assign(Course);
+                Trace.WriteLine("Usao da moze da create za " + Course.ToCourse().TutorId);
+                AssignTutor(Course);
                 courseService.Add(Course.ToCourse());
                 MessageBox.Show("Success!");
                 return true;
@@ -39,15 +35,15 @@ namespace LangLang.WPF.ViewModels.CourseViewModels
             MessageBox.Show("The course cannot be created, there are time overlaps or no available classroms (if the course is held in a classroom).");
             return false;
         }
-        /*
-        private void Assign(CourseViewModel course)
+        
+        private void AssignTutor(CourseViewModel course)
         {
-            course.TutorId = MostSuitableTutor(Course.ToCourse());
+            course.TutorId = SmartSystem.MostSuitableTutor(Course.ToCourse());
             if(course.TutorId == -1)
             {
                 MessageBox.Show("There is no suitable tutor for this course at the moment.");
             }
         }
-        */
+        
     }
 }
