f5aca146cc0fc0e4ffb56dc45e4d813a9d13cfcb
Natasa Kasikovic~ <nkasikovic@yahoo.com>
2024-06-12 17:28:52 +0200

    Fix: database migartions issues

---
+++ LangLang/Migrations/20240612152547_Initial.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace LangLang.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Course",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    TutorId = table.Column<int>(type: "integer", nullable: false),
+                    Language = table.Column<string>(type: "text", nullable: false),
+                    Level = table.Column<int>(type: "integer", nullable: false),
+                    NumberOfWeeks = table.Column<int>(type: "integer", nullable: false),
+                    Days = table.Column<int[]>(type: "integer[]", nullable: false),
+                    Online = table.Column<bool>(type: "boolean", nullable: false),
+                    NumberOfStudents = table.Column<int>(type: "integer", nullable: false),
+                    MaxStudents = table.Column<int>(type: "integer", nullable: false),
+                    StartDateTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    CreatedByDirector = table.Column<bool>(type: "boolean", nullable: false),
+                    Modifiable = table.Column<bool>(type: "boolean", nullable: false),
+                    GratitudeEmailSent = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Course", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ExamSlot",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Language = table.Column<string>(type: "text", nullable: false),
+                    Level = table.Column<int>(type: "integer", nullable: false),
+                    TutorId = table.Column<int>(type: "integer", nullable: false),
+                    MaxStudents = table.Column<int>(type: "integer", nullable: false),
+                    Applicants = table.Column<int>(type: "integer", nullable: false),
+                    Modifiable = table.Column<bool>(type: "boolean", nullable: false),
+                    ResultsGenerated = table.Column<bool>(type: "boolean", nullable: false),
+                    ExamineesNotified = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExamSlot", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TimeSlot",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Duration = table.Column<double>(type: "double precision", nullable: false),
+                    Time = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    CourseId = table.Column<int>(type: "integer", nullable: true),
+                    ExamId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TimeSlot", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TimeSlot_Course_CourseId",
+                        column: x => x.CourseId,
+                        principalTable: "Course",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TimeSlot_ExamSlot_ExamId",
+                        column: x => x.ExamId,
+                        principalTable: "ExamSlot",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TimeSlot_CourseId",
+                table: "TimeSlot",
+                column: "CourseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TimeSlot_ExamId",
+                table: "TimeSlot",
+                column: "ExamId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TimeSlot");
+
+            migrationBuilder.DropTable(
+                name: "Course");
+
+            migrationBuilder.DropTable(
+                name: "ExamSlot");
+        }
+    }
+}
