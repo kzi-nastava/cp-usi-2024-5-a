4aa05c509809c0ef646d8274d4b2ebd9e2b3b7d7
DusicaPesic <130140285+DusicaPesic@users.noreply.github.com>
2024-06-14 22:25:06 +0200

    [Add] course console for tutor

---
+++ LangLang/ConsoleApp/View/TutorView/CoursesView.cs
@@ -0,0 +1,25 @@
+﻿using LangLang.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LangLang.ConsoleApp.View.TutorView
+{
+    public class CoursesView
+    {
+
+        public void DisplayCourses(List<Course> courses)
+        {
+            var table = new GenericTable<Course>(courses, true);
+            table.DisplayTable();
+
+            // You can add more functionality here, like selecting a course
+            // Example:
+            // var selectedCourse = table.SelectRow();
+            // Console.WriteLine($"Selected course: {selectedCourse?.Name}");
+        }
+
+    }
+}
