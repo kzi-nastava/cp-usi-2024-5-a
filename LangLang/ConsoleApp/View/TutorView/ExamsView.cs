f749fea9704e630714a4d66f57cee19eaae666b6
DusicaPesic <130140285+DusicaPesic@users.noreply.github.com>
2024-06-14 22:25:36 +0200

    [Add] console for tutors exams

---
+++ LangLang/ConsoleApp/View/TutorView/ExamsView.cs
@@ -0,0 +1,181 @@
+﻿using LangLang.BusinessLogic.UseCases;
+using LangLang.Configuration;
+using LangLang.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace LangLang.ConsoleApp.View.TutorView
+{
+    public class ExamsView
+    {
+        private List<ExamSlot> exams { get; set; }
+        private Tutor tutor {  get; set; }
+        public ExamsView(Tutor loggedIn)
+        {
+            tutor = loggedIn;
+            ReloadExams();
+            Run();
+        }
+        public void Run()
+        {
+            bool inExams = true;
+            while (inExams)
+            {
+                Console.WriteLine("-----------------------------------------------------------------------");
+                ReloadExams();
+                DisplayExams();
+
+                Console.WriteLine("Exam Menu:");
+                Console.WriteLine("1. Create Exam");
+                Console.WriteLine("2. Update Exam");
+                Console.WriteLine("3. Delete Exam");
+                Console.WriteLine("4. Exit");
+                Console.Write("Enter your choice: ");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        CreateExam();
+                        break;
+                    case "2":
+                        UpdateExam();
+                        break;
+                    case "3":
+                        DeleteExam();
+                        break;
+                    case "4":
+                        Console.WriteLine("Exiting...");
+                        return;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("-----------------------------------------------------------------------");
+
+            }
+        }
+        public void DisplayExams()
+        {
+            var table = new GenericTable<ExamSlot>(exams, true);
+            table.DisplayTable();
+        }
+
+        public void CreateExam()
+        {
+            Console.WriteLine("Creating new exam...");
+            ExamSlot exam = GenericForm.CreateEntity<ExamSlot>();
+            exam.CreatedAt = DateTime.Now;
+            exam.TutorId = tutor.Profile.Id;
+            exam.Modifiable = true;
+
+            if (!IsValid(exam)) { 
+                Console.WriteLine("Exam slot can not be created. Not all fields are valid.");
+                return;
+            }
+
+            ExamSlotService service = new();
+            bool added = service.Add(exam);
+
+            if (!added) {
+                MessageBox.Show("Choose another exam date or time.");
+                return; 
+            }
+
+            Console.WriteLine("Exam created successfully.");
+            ReloadExams();
+        }
+        //FIX THIS
+        private bool IsValid(ExamSlot exam)
+        {
+            return false;
+        }
+        public void UpdateExam()
+        {
+            Console.WriteLine("Updating exam...");
+            var table = new GenericTable<ExamSlot>(exams, true);
+            ExamSlot selected = table.SelectRow();
+            //no exam was selected
+            if (selected == null) return;
+
+            //returns if selected exam can not be updated
+            ExamSlotService service = new();
+            if (!service.CanBeUpdated(selected))
+            {
+                MessageBox.Show($"Can't update exam, there is less than {Constants.EXAM_MODIFY_PERIOD} days before exam or exam has passed.");
+                return;
+            }
+
+            ExamSlot updated = selected;            
+            Console.WriteLine("Updating exam details:");
+            updated = GenericForm.UpdateEntity<ExamSlot>(selected);
+
+            if (!service.CanCreateExam(updated))
+            {
+                Console.Write($"Exam can not be updated. You must choose another exams date or time.");
+                return;
+            }
+            service.Update(updated);
+            Console.WriteLine("Exam updated successfully.");
+        }
+
+        public void DeleteExam()
+        {
+            Console.WriteLine("Deleting exam...");
+            var table = new GenericTable<ExamSlot>(exams, true);
+            ExamSlot selected = table.SelectRow();
+            if (selected == null) return;
+
+            bool confirmed = ConfirmationMessage();
+            if (confirmed)
+            {
+                ExamSlotService service = new();
+                if (!service.Delete(selected.Id))
+                {
+                    Console.Write($"Can't delete exam, there is less than {Constants.EXAM_MODIFY_PERIOD} days before exam.");
+                }
+                else
+                {
+                    Console.Write("Exam successfully deleted.");
+                }
+            }
+
+        }
+
+        private void ReloadExams()
+        {
+            ExamSlotService service = new();
+            exams = service.GetExams(tutor);
+        }
+        private bool ConfirmationMessage()
+        {
+            Console.WriteLine("Are you sure?");
+            Console.WriteLine("1. Yes");
+            Console.WriteLine("2. No");
+
+            Console.Write("Enter your choice (1/2): ");
+            string choice = Console.ReadLine();
+            while (true)
+            {
+                switch (choice)
+                {
+                    case "1":
+                        return true;
+                    case "2":
+                        return false;
+                    default:
+                        Console.WriteLine("Invalid choice try again.");
+                        break;
+                }
+            }
+            
+        }
+    }
+}
