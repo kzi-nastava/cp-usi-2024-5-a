8af5d719fd182a962e4de3bf6bb1b8a15245d8bc
DusicaPesic <130140285+DusicaPesic@users.noreply.github.com>
2024-06-14 22:22:20 +0200

    [Add] attributes needed for generic tables and forms

---
+++ LangLang/ConsoleApp/Attributes/Show.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace LangLang.ConsoleApp.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class Show : Attribute
+    {
+        // You can add additional properties or methods if needed
+    }
+}
