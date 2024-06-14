452230d2bb68a35fdb1dcfadebb74452ee88d648
Ana Šinik <sinikana7@gmail.com>
2024-06-14 21:38:58 +0200

    feat: link TutorSkill entity to the database

---
+++ LangLang/Domain/Models/TutorSkill.cs
@@ -0,0 +1,19 @@
+﻿
+namespace LangLang.Domain.Models
+{
+    public class TutorSkill
+    {
+        public int Id { get; set; }
+        public int TutorId { get; set; }
+        public int LanguageLevelId { get; set; }
+
+        public TutorSkill() { }
+
+        public TutorSkill(int id, int tutorId, int languageLevelId)
+        {
+            Id = id;
+            TutorId = tutorId;
+            LanguageLevelId = languageLevelId;
+        }
+    }
+}
