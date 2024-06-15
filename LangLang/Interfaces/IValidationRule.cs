e107cfd64770150f39a8efd22fac735a61889f13
DusicaPesic <130140285+DusicaPesic@users.noreply.github.com>
2024-06-15 16:00:33 +0200

    [Add] interface IValidationRule used for implementing open closed principle for validating fields in view models

---
+++ LangLang/Interfaces/IValidationRule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LangLang.Interfaces
+{
+    public interface IValidationRule<T>
+    {
+        string ErrorMessage { get; }
+        bool Validate(T value);
+    }
+}
