ec20968c9716faa7b0e7c23cb0e9790cbf5bcfaa
Darinka Loncar <149894872+darinkaloncar@users.noreply.github.com>
2024-06-15 17:46:21 -0700

    [Update] generic forms

--- LangLang/ConsoleApp/GenericStructures/GenericForm.cs
+++ LangLang/ConsoleApp/GenericStructures/GenericForm.cs
@@ -1,9 +1,12 @@
 ﻿using LangLang.ConsoleApp.Attributes;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using System.Reflection;
 using System.Windows;
+using System.Windows.Documents;
 
 namespace LangLang.ConsoleApp.GenericStructures
 {
@@ -18,120 +21,60 @@ namespace LangLang.ConsoleApp.GenericStructures
             {
                 if (!Attribute.IsDefined(property, typeof(AllowCreate))) continue;
 
-                if (Attribute.IsDefined(property, typeof(CourseDays)))
-                {
-                    List<DayOfWeek> days = inputDays();
-                    property.SetValue(entity, days);
-                    continue;
-                }
-
                 var displayNameAttribute = property.GetCustomAttribute<DisplayNameAttribute>();
                 string displayName = displayNameAttribute != null ? displayNameAttribute.DisplayName : property.Name;
                 Type pType = property.PropertyType;
-                if (pType.IsClass && pType != typeof(string))
+                if (pType.IsGenericType && pType.GetGenericTypeDefinition() == typeof(List<>))
+                {
+                    Console.WriteLine($"Enter values for {displayName} (press Enter to finish):");
+                    Type elementType = pType.GetGenericArguments()[0];
+                    IList list = (IList)Activator.CreateInstance(pType);
+                    bool first = true;
+                    while (true)
+                    {
+                        Console.Write($"Enter {elementType.Name} value: ");
+                        string input = Console.ReadLine();
+                        if (string.IsNullOrEmpty(input) && first == false) break;
+
+                        if (TryParseInput(elementType, input, out object elementValue))
+                        {
+                            if (first) { first = false; }
+                            list.Add(elementValue);
+                            continue;
+                        }
+                        if (first)
+                        {
+                            Console.WriteLine($"Must enter at least one valid element for {elementType.Name}.");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Invalid input. Please enter a valid {elementType.Name}.");
+                        }
+                    }
+
+                    property.SetValue(entity, list);
+                    continue;
+                }
+                else if (pType.IsClass && pType != typeof(string))
                 {
                     Console.WriteLine($"Creating a new instance for {property.Name}:");
                     var nestedEntity = CreateEntityForType(pType);
                     property.SetValue(entity, nestedEntity);
                     continue;
                 }
-                bool success = false;
-                while ( !success )
+                while (true)
                 {
                     Console.WriteLine($"Enter value for {displayName} ({property.PropertyType.Name}):");
                     string input = Console.ReadLine();
-                    success = CheckInput(property, input, entity);
-
+                    if(TryParseInput(property.PropertyType, input, out object value))
+                    {
+                        property.SetValue(entity, value);
+                        break;
+                    }
                 }
             }
 
             return entity;
-        }
-        private static bool CheckInput<T>(PropertyInfo property, string input, T entity) {
-            Type pType = property.PropertyType;
-            // Check if the property type is DateTime
-
-            if (pType == typeof(DateTime))
-            {
-                string format = "HH:mm dd.MM.yyyy";
-                if (DateTime.TryParseExact(input, format, null, System.Globalization.DateTimeStyles.None, out DateTime dateTimeValue))
-                {
-                    if (dateTimeValue <= DateTime.Now)
-                    {
-                        Console.WriteLine("The date and time are not in the future.");
-                        return false;
-                    }
-                    property.SetValue(entity, dateTimeValue);
-                    return true;
-                }
-                Console.WriteLine($"Invalid input. Please enter a valid DateTime for {property.Name} in format HH:mm dd.MM.yyyy");
-                return false;
-
-            }
-            else if (pType == typeof(string))
-            {
-                property.SetValue(entity, input);
-                return true;
-            }
-            else if (pType == typeof(int))
-            {
-                if (int.TryParse(input, out int intValue))
-                {
-                    if(intValue <= 0)
-                    {
-                        Console.WriteLine("The integer value must be greater than 0.");
-                        return false;
-                    }
-                    property.SetValue(entity, intValue);
-                    return true;
-                }
-                Console.WriteLine($"Invalid input. Please enter a valid integer for {property.Name}.");
-                return false;
-            }
-            else if (pType == typeof(bool))
-            {
-                if (bool.TryParse(input, out bool boolValue))
-                {
-                    property.SetValue(entity, boolValue);
-                    return true;
-                }
-                Console.WriteLine($"Invalid input. Please enter 'true' or 'false' for {property.Name}.");
-                return false;
-            }
-            else if (pType.IsEnum)
-            {
-                if (Enum.TryParse(pType, input, true, out object enumValue))
-                {
-                    property.SetValue(entity, enumValue);
-                    return true;
-                }
-                Console.WriteLine($"Invalid input. Please enter a valid value for {property.Name} from the options: {string.Join(", ", Enum.GetNames(pType))}.");
-                return false;
-
-            }else if (pType == typeof(double))
-            {
-                if (double.TryParse(input, out double doubleValue))
-                {
-                    property.SetValue(entity, doubleValue);
-                    return true;
-                }
-                Console.WriteLine($"Invalid input. Please enter a valid double for {property.Name}.");
-                return false;
-            }
-            else
-            {
-                try
-                {
-                    object value = Convert.ChangeType(input, property.PropertyType);
-                    property.SetValue(entity, value);
-                    return true;
-                }
-                catch (InvalidCastException)
-                {
-                    Console.WriteLine($"Invalid input. Cannot convert {input} to {property.PropertyType.Name} for {property.Name}.");
-                    return false;
-                }
-            }
         }
         
         public static object CreateEntityForType(Type type)
@@ -152,9 +95,17 @@ namespace LangLang.ConsoleApp.GenericStructures
                     object currentValue = property.GetValue(entity);
                     var displayNameAttribute = property.GetCustomAttribute<DisplayNameAttribute>();
                     string displayName = displayNameAttribute != null ? displayNameAttribute.DisplayName : property.Name;
-                    if (Attribute.IsDefined(property, typeof(CourseDays)))
+                    
+                    if (property.PropertyType.IsGenericType && property.PropertyType.GetGenericTypeDefinition() == typeof(List<>))
                     {
-                        currentValue = string.Join(", ", (List<DayOfWeek>)currentValue);
+                        var list = (System.Collections.IList)currentValue;
+                        string enumValues = string.Join(", ", list.Cast<object>());
+                        currentValue = enumValues;
+                    }
+                    else if (property.PropertyType.IsClass && property.PropertyType != typeof(string))
+                    {
+                        PropertyInfo[] properties2 = (property.PropertyType).GetProperties();
+                        currentValue = string.Join(", ", properties2.Cast<object>());
                     }
                     Console.WriteLine($"Current value for {displayName}: {currentValue}");
 
@@ -163,26 +114,55 @@ namespace LangLang.ConsoleApp.GenericStructures
 
                     if (choice.ToUpper() == "Y")
                     {
-                        if (Attribute.IsDefined(property, typeof(CourseDays)))
+                        Type pType = property.PropertyType;
+                        if (pType.IsGenericType && pType.GetGenericTypeDefinition() == typeof(List<>))
                         {
-                            List<DayOfWeek> days = inputDays();
-                            property.SetValue(entity, days);
+                            Console.WriteLine($"Enter values for {displayName} (press Enter to finish):");
+                            Type elementType = pType.GetGenericArguments()[0];
+                            IList list = (IList)Activator.CreateInstance(pType);
+
+                            bool first = true;
+                            while (true)
+                            {
+                                Console.Write($"Enter {elementType.Name} value: ");
+                                string input = Console.ReadLine();
+                                if (string.IsNullOrEmpty(input) && first == false) break;
+
+                                if (TryParseInput(elementType, input, out object elementValue))
+                                {
+                                    if (first) { first = false; }
+                                    list.Add(elementValue);
+                                    continue;
+                                }
+                                if (first)
+                                {
+                                    Console.WriteLine($"Must enter at least one valid element for {elementType.Name}.");
+                                }
+                                else
+                                {
+                                    Console.WriteLine($"Invalid input. Please enter a valid {elementType.Name}.");
+                                }
+                            }
+
+                            property.SetValue(entity, list);
                             continue;
                         }
-                        Type pType = property.PropertyType;
-                        if (pType.IsClass && pType != typeof(string))
+                        else if (pType.IsClass && pType != typeof(string))
                         {
                             Console.WriteLine($"Creating a new instance for {property.Name}:");
                             var nestedEntity = CreateEntityForType(pType);
                             property.SetValue(entity, nestedEntity);
                             continue;
                         }
-                        bool success = false;
-                        while (!success)
+                        while (true)
                         {
                             Console.WriteLine($"Enter value for {displayName} ({property.PropertyType.Name}):");
                             string valueInput = Console.ReadLine();
-                            success = CheckInput(property, valueInput, entity);
+                            if (TryParseInput(property.PropertyType, valueInput, out object value))
+                            {
+                                property.SetValue(entity, value);
+                                break;
+                            }
                         }
                     }
                 }
@@ -190,55 +170,84 @@ namespace LangLang.ConsoleApp.GenericStructures
 
             return entity;
         }
-        private static List<DayOfWeek> inputDays()
+        private static bool TryParseInput(Type elementType, string input, out object result)
         {
-            Console.WriteLine($"Input the number of days on which the course will be held (between 1 and 5):");
-            string input = Console.ReadLine();
-            while (true)
+            result = null;
+            if (elementType == typeof(DateTime))
             {
-                switch (input)
+                string format = "HH:mm dd.MM.yyyy";
+                if (DateTime.TryParseExact(input, format, null, System.Globalization.DateTimeStyles.None, out DateTime dateTimeValue))
                 {
-                    case "1":
-                    case "2":
-                    case "3":
-                    case "4":
-                        return selectedDays(int.Parse(input));
-                    case "5":
-                        Console.WriteLine("You have selected all of the days");
-                        return new List<DayOfWeek> { DayOfWeek.Monday, DayOfWeek.Tuesday, DayOfWeek.Wednesday, DayOfWeek.Thursday, DayOfWeek.Friday };
-                    default:
-                        Console.WriteLine("Invalid choice try again.");
-                        break;
+                    result = dateTimeValue;
+                    return true;
+                }
+                Console.WriteLine($"Invalid input. Please enter a valid DateTime in format HH:mm dd.MM.yyyy");
+                return false;
+            }
+            else if (elementType == typeof(string))
+            {
+                result = input;
+                return true;
+            }
+            else if (elementType == typeof(int))
+            {
+                if (int.TryParse(input, out int intValue))
+                {
+                    if (intValue <= 0)
+                    {
+                        Console.WriteLine("The integer value must be greater than 0.");
+                        return false;
+                    }
+                    result = intValue;
+                    return true;
+                }
+                Console.WriteLine($"Invalid input. Please enter a valid integer.");
+                return false;
+            }
+            else if (elementType == typeof(bool))
+            {
+                if (bool.TryParse(input, out bool boolValue))
+                {
+                    result = boolValue;
+                    return true;
+                }
+                Console.WriteLine($"Invalid input. Please enter 'true' or 'false'.");
+                return false;
+            }
+            else if (elementType.IsEnum)
+            {
+                if (Enum.TryParse(elementType, input, true, out object enumValue))
+                {
+                    result = enumValue;
+                    return true;
+                }
+                Console.WriteLine($"Invalid input. Please enter a valid value from the options: {string.Join(", ", Enum.GetNames(elementType))}.");
+                return false;
+
+            }
+            else if (elementType == typeof(double))
+            {
+                if (double.TryParse(input, out double doubleValue))
+                {
+                    result = doubleValue;
+                    return true;
+                }
+                Console.WriteLine($"Invalid input. Please enter a valid double.");
+                return false;
+            }
+            else
+            {
+                try
+                {
+                    object value = Convert.ChangeType(input, elementType);
+                    return true;
+                }
+                catch (InvalidCastException)
+                {
+                    Console.WriteLine($"Invalid input. Cannot convert {input} to {elementType.Name}.");
+                    return false;
                 }
             }
-        }
-        private static List<DayOfWeek> selectedDays(int num)
-        {
-            List<DayOfWeek> result = new List<DayOfWeek>();
-            for (int i = 1; i <= num; i++)
-            {
-                Console.WriteLine($"Input day {i}:");
-                string input = Console.ReadLine();
-                while (true)
-                {
-                    bool success = Enum.TryParse<DayOfWeek>(input, true, out DayOfWeek day);
-                    if (success && result.Contains(day) == false)
-                    {
-                        result.Add(day);
-                        break;
-                    }
-                    if (success)
-                    {
-                        Console.WriteLine("You have already selected that day. Try again: ");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Invalid input. Try again: ");
-                    }
-                    input = Console.ReadLine();
-                }
-            }
-            return result;
         }
     }
 }
